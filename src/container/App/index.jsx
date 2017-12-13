da69827d3ff07eed17738e171e4e56a00ec6fd6a
Isac <isac_asj91@hotmail.com>
2017-12-13 21:52:59 -0200

    add index and app

---
+++ src/container/App/index.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+class App extends PureComponent {
+  render () {
+    return (
+      <>
+        <h1>React Dipeex Boilerplate + Fiber + Fragment + Webpack 3</h1>
+      </>
+    )
+  }
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    app: './src/index.jsx'
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js',
+    publicPath: '/'
+  },
+
+  resolve: {
+    alias: {
+      container: path.resolve(__dirname, './src/container')
+    },
+    modules: [path.resolve(__dirname, 'node_modules')],
+    extensions: ['.jsx', '.js']
+  },
+
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      { test: /\.(js|jsx)?$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'React Boilerplate',
+      template: './src/index.ejs'
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: ({ resource }) => /node_modules/.test(resource)
+    })
+  ]
+}
